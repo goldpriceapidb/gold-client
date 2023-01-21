02b0416d887f34b8544272b52d467ffe032d8345
tharunoptimus <tharunoptimus@outlook.com>
2023-01-21 15:31:06 +0530

    added `updatedLastFetch`

--- src/components/LastFetched.tsx
+++ src/components/LastFetched.tsx
@@ -1,3 +1,10 @@
+
+async function updatedLastFetch() {
+    let time = new Date().getTime()
+    await set(IDB_LAST_UPDATED_TIME, time)
+    return time
+}
+
 async function updatePrice() {
     let request = await fetch(FETCH_URL)
     request = await request.json()
