0bfcaf738f7106a468523188ed08ffb276e63876
tharunoptimus <tharunoptimus@outlook.com>
2023-01-21 15:35:15 +0530

    `CountryList` now fetches countries from IDB

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -5,9 +5,33 @@ import Country from "./Country"
 
 export default function CountryList(): JSX.Element {
 
+    let [contents, setContents] = useState([])
+    
+    
+    useEffect(() => {
+        
+        async function fetchStuff() {
+            let countries = await get(IDB_GOLD_PRICE_STORAGE_KEY)
+            setContents(countries)
+        }
+
+
+        fetchStuff()
+
+    }, [])
+
 	return <>
     
 
+    {
+        contents !== undefined &&
+        contents.length !== 0 &&
+        contents.map( (e: CountryType)  => (
+            <Country country={e} key={e._id}/>
+        ))
+    }
+
+    
     </>
 
 }
