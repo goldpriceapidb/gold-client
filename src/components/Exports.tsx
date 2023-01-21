4424120ee18b28dcc2a32e1fb35acb1938058802
tharunoptimus <tharunoptimus@outlook.com>
2023-01-21 15:30:16 +0530

    Added `CountryType`

--- src/components/Exports.tsx
+++ src/components/Exports.tsx
@@ -5,5 +5,21 @@ export default function Exports(): JSX.Element {
 const FETCH_URL = "https://gold-price.cyclic.app/api/country/all"
 const IDB_GOLD_PRICE_STORAGE_KEY = "goldData"
 const IDB_LAST_UPDATED_TIME = "lastUpdated"
+export type CountryType = {
+    _id:                    string;
+    countryName:            string;
+    countryCode:            string;
+    currency:               string;
+    currentPrice:           number;
+    goldLastUpdated:        Date;
+    currencyConversionRate: number;
+    createdAt:              Date;
+    updatedAt:              Date;
+    __v:                    number;
+    previousPrice:          number;
+    priceChange:            number;
+    priceChangePercentage:  number;
+}
+
 
 export { FETCH_URL, IDB_GOLD_PRICE_STORAGE_KEY, IDB_LAST_UPDATED_TIME }
