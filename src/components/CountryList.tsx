ef27122ad73d4276cc97d3d2d3d7d9d13396f9cd
tharunoptimus <tharunoptimus@outlook.com>
2023-01-21 15:33:45 +0530

    Added `CountryList` component

---
+++ src/components/CountryList.tsx
@@ -0,0 +1,13 @@
+import { get, set } from "idb-keyval"
+import { useEffect, useState } from "react"
+import { IDB_GOLD_PRICE_STORAGE_KEY, FETCHED_DATA, CountryType } from "./Exports"
+import Country from "./Country"
+
+export default function CountryList(): JSX.Element {
+
+	return <>
+    
+
+    </>
+
+}
