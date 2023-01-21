3509a4f76c89b67caa7f80b76d63c38d1f3860ef
tharunoptimus <tharunoptimus@outlook.com>
2023-01-21 17:59:17 +0530

    Added styles for `Country` module

--- src/components/Country/index.tsx
+++ src/components/Country/index.tsx
@@ -9,37 +9,25 @@ export default function Country(props: ExportedProps): JSX.Element {
 	let stuff = props.country
 	return (
 		<>
-			<div
-				style={{
-					border: "1px solid black",
-					borderRadius: "12px",
-					margin: "1rem",
-					padding: "1rem",
-					display: "flex",
-					flexDirection: "column",
-				}}
-			>
-				<p>{stuff.countryCode}</p>
-				<p>{stuff.countryName}</p>
-				<p>{stuff.currency}</p>
-				<p>{stuff.currencyConversionRate}</p>
-				<p>{stuff.currentPrice}</p>
+			<div className={styles.countryCard}>
+				<span className={styles.countryName}>
+					<p>{stuff.countryName}</p>
+					<p className={styles.countryCode}>({stuff.countryCode})</p>
+				</span>
 				<p>
+					{stuff.currentPrice} {stuff.currency}
+				</p>
+				<p className={styles.lastUpdated}>
+					{"Last Updated: "}
 					{new Date(stuff.goldLastUpdated).toLocaleString("en-US", {
 						timeZone: "UTC",
 						dateStyle: "medium",
 						timeStyle: "medium",
 					})}
 				</p>
-				<p>{stuff.previousPrice}</p>
-				<p>{stuff.priceChange}</p>
-				<p>{stuff.priceChangePercentage}</p>
-				<p>
-					{new Date(stuff.updatedAt).toLocaleString("en-US", {
-						timeZone: "UTC",
-						dateStyle: "medium",
-						timeStyle: "medium",
-					})}
+				<p className={styles.percentageIncrease}>
+					{stuff.priceChangePercentage.toFixed(2)}
+					{"%"}
 				</p>
 			</div>
 		</>
