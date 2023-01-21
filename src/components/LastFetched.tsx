6ad136f4715815ae0f7f321b19b9037b9008a924
tharunoptimus <tharunoptimus@outlook.com>
2023-01-21 15:31:22 +0530

    added `timeDifference`

--- src/components/LastFetched.tsx
+++ src/components/LastFetched.tsx
@@ -11,3 +11,30 @@ async function updatePrice() {
     await set(IDB_GOLD_PRICE_STORAGE_KEY, request)
     return
 }
+
+function timeDifference(current: number, previous: number): string {
+    let msPerMinute = 60 * 1000
+    let msPerHour = msPerMinute * 60
+    let msPerDay = msPerHour * 24
+    let msPerMonth = msPerDay * 30
+    let msPerYear = msPerDay * 365
+
+    let elapsed = current - previous
+    let trailingWord = " ago"
+
+    if (elapsed < msPerMinute) {
+        if (elapsed / 1000 < 5) return "Just now"
+        return `${Math.round(elapsed / 1000)}s ${trailingWord}`
+    } else if (elapsed < msPerHour) {
+        return `${Math.round(elapsed / msPerMinute)}m ${trailingWord}`
+    } else if (elapsed < msPerDay) {
+        return `${Math.round(elapsed / msPerHour)}h ${trailingWord}`
+    } else if (elapsed < msPerMonth) {
+        return `${Math.round(elapsed / msPerDay)}d ${trailingWord}`
+    } else if (elapsed < msPerYear) {
+        return `${Math.round(elapsed / msPerMonth)}month ${trailingWord}`
+    } else {
+        return `${Math.round(elapsed / msPerYear)}y ${trailingWord}`
+    }
+}
+
